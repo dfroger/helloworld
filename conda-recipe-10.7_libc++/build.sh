b1b6cea8527c58a8205d233fe7393807380099b2
David Froger <david.froger@inria.fr>
2015-11-03 13:32:01 -0800

    Add required flag for 10.7

--- conda-recipe-10.7_libc++/build.sh
+++ conda-recipe-10.7_libc++/build.sh
@@ -3,7 +3,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.7
 # Build
 make \
     CXX=clang++ \
-    CXXFLAGS=-stdlib=libc++ \
-    LDFLAGS=-stdlib=libc++
+    CXXFLAGS=-stdlib="libc++ -mmacosx-version-min=10.7"\
+    LDFLAGS=-stdlib="libc++ -mmacosx-version-min=10.7"
 
 make install
--- conda-recipe-10.7_libstdc++/build.sh
+++ conda-recipe-10.7_libstdc++/build.sh
@@ -3,7 +3,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.7
 # Build
 make \
     CXX=clang++ \
-    CXXFLAGS=-stdlib=libstdc++ \
-    LDFLAGS=-stdlib=libstdc++
+    CXXFLAGS=-stdlib="libstdc++ -mmacosx-version-min=10.7"\
+    LDFLAGS=-stdlib="libstdc++ -mmacosx-version-min=10.7"
 
 make install
