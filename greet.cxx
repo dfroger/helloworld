40c8a720dd2ac5d5f8cd91a0d3ee5abd1b44734c
David Froger <david.froger@inria.fr>
2015-11-03 13:38:11 -0800

    Add C++11 features

--- greet.cxx
+++ greet.cxx
@@ -6,7 +6,13 @@ namespace Helloworld
 std::string
 greet(std::string name)
 {
-    return (std::string) "Hello, " + name + "!";
+#ifdef HAS_TO_STRING
+    auto greeting = "Hello, " + name + "! "
+                  + "Here is a number: " + std::to_string( 0.123);
+#else
+    auto greeting = "Hello, " + name + "! ";
+#endif
+    return greeting;
 }
 
 }
